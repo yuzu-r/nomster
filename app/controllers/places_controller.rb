8874d10be4b5753a3a2c2ff43211f91ba6ff8eb6
Tracy Kaneshiro <tkaneshiro@gmail.com>
2016-04-15 16:06:42 +0000

    destroy function

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -22,6 +22,17 @@ class PlacesController < ApplicationController
     @place=Place.find(params[:id])
   end
 
+  def update
+    @place=Place.find(params[:id])
+    @place.update_attributes(place_params)
+    redirect_to root_path
+  end
+
+  def destroy
+    @place=Place.find(params[:id])
+    @place.destroy
+    redirect_to root_path
+  end
 
   private
     def place_params
