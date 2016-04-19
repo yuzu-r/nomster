272800c6bb4735c14f906f7169016345e2473b4c
Tracy Kaneshiro <tkaneshiro@gmail.com>
2016-04-19 17:44:42 +0000

    configure production email for sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,15 @@ Nomster::Application.configure do
 
   # Devise
   config.action_mailer.default_url_options = { host: 'nomster-yuzu-r.herokuapp.com'}
+
+  # Emails
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+    }  
 end
