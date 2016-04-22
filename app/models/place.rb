cec954a2df314a56ad133e8a8133b15285adcdc0
Tracy Kaneshiro <tkaneshiro@gmail.com>
2016-04-22 15:15:38 +0000

    delete pics, comments when place is deleted

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,7 +1,7 @@
 class Place < ActiveRecord::Base
   belongs_to :user
-  has_many :comments
-  has_many :photos
+  has_many :comments, dependent: :destroy
+  has_many :photos, dependent: :destroy
   validates :name, presence: true, length: { minimum: 3}
   validates :address, presence: true
   validates :description, presence: true
